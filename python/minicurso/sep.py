a99b63458ff148c2175b2ffac715d118c7832971
Victor Emanuel <55245500+vicgomes@users.noreply.github.com>
2019-12-19 14:52:36 -0300

    Add files via upload

--- python/minicurso/sep.py
+++ python/minicurso/sep.py
@@ -2,10 +2,10 @@ dia = 7
 mes = 2
 ano = 2002
 
-print(dia, "/", mes, "/", ano)                 # método bruto
-
-print(dia, mes, ano, sep = "/")                # no 'sep' você escolhe o que deseja para separar as variáveis
 print('\n')
+print(dia, "/", mes, "/", ano)                             # método bruto
 
-print('{}/{}/{}'.format(dia, mes, ano))        # '{}' serve pra reservar o espaço que será utilizado pra jogar um valor no .format
+print(dia, mes, ano, sep = "/")                            # no 'sep' você escolhe o que deseja para separar as variáveis
+
+print('{:02.0f}/{:02.0f}/{}'.format(dia, mes, ano))        # '{}' serve pra reservar o espaço que será utilizado pra jogar um valor no .format
 print('\n')
