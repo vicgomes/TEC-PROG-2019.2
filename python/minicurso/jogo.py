87e6d928a523d667615bfed8ccfc845ecea30f08
Victor Emanuel <55245500+vicgomes@users.noreply.github.com>
2019-12-19 14:25:27 -0300

    Add files via upload

--- python/minicurso/jogo.py
+++ python/minicurso/jogo.py
@@ -3,15 +3,28 @@ print('eu to morrendo de sono')
 print('AAAAAAAAAAAAAAAAAAAAAA')
 
 num_baitola = 69
+tentativas = 3
+rodadas = 1
 
-chute = int(input('\ntenta aí seu babaca, é de 0-100: '))
+print('\né de 0-100 e tu tem 3 chances mizera')
 
-if chute == num_baitola:
-    print('\npelo menos nisso tu acerta...')
-else:
-    if chute > num_baitola:
-        print('\nFOI LONGE PRA PORRA BOY, tai vissano é?')
-        print('tu não cansa de ser um fracasso?')
-    elif chute < num_baitola:                                      # o elif dá um break em tudo a partir do momento que sua condição é satisfeita
-        print('\nmenor que isso só teu pau!')
-        print('tu não cansa de ser um fracasso?')
+while tentativas >= rodadas:
+    print('\n', rodadas, 'de', tentativas)
+    chute = int(input('tenta aí seu babaca: '))
+
+    if chute == num_baitola:
+        print('\npelo menos nisso tu acerta...')
+        break
+    else:
+        if chute > num_baitola:
+            print('\nFOI LONGE PRA PORRA BOY, tai vissano é?')
+            print('tu não cansa de ser um fracasso?')
+            break
+        if chute < num_baitola:                                      # o elif dá um break em tudo a partir do momento que sua condição é satisfeita
+            print('\nmenor que isso só teu pau!')
+            print('tu não cansa de ser um fracasso?')
+            break
+    
+    rodadas = rodadas +1
+
+print('carai tu é um fudido mesmo viu? já era :/')
